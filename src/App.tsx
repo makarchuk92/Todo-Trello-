557bceb3a59edc2a8ad728094e929c2f89b034bf
Andrei <andrei.nakarchuk@gmail.com>
2024-01-03 22:35:53 +0300

    Aded chekbox in Todo component, added new todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,53 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TodoList from './Components/TodoList/TodoList';
-import { Todo } from './Components/Todo';
+import { TaskType, Todo } from './Components/Todo';
+import { text } from 'stream/consumers';
 
+
+
+export type FilterValuesType = 'all' | 'active' | 'completed'
 
 function App() {
   const todos = [
-    { id: 1, title: 'test1', isDone: true },
-    { id: 2, title: 'test2', isDone: false},
-    { id: 3, title: 'test3', isDone: true}
+    { id: 1, text: 'test1', isDone: true },
+    { id: 2, text: 'test2', isDone: false },
+    { id: 3, text: 'test3', isDone: true }
   ]
 
-  const [task, setTask] = useState(todos)
+  const [task, setTask] = useState<Array<TaskType>>(todos)
+  const [filter, setFilter] = useState<FilterValuesType>('all')
+  const [input, setInput] = useState<string>('')
 
+  const addTodo = () => {
+
+    const newTodo = {
+      text: input,
+      isDone: false,
+      id: Date.now()
+    }
+    setTask([...task, newTodo])
+    
+  }
   const removeTodo = (id: number) => {
-    const filterTodo = task.filter( t => t.id !== id)
-    setTask(filterTodo)
+    setTask(prev => prev.filter(todo => todo.id !== id))
+  }
+
+  const changeFilter = (value: FilterValuesType) => {
+    setFilter(value)
+  }
+
+  let taskForTodo = task
+  if (filter === 'completed') {
+    taskForTodo = task.filter(t => t.isDone === true)
+  }
+  if (filter === 'active') {
+    taskForTodo = task.filter(t => t.isDone === false)
   }
 
   return (
     <div className="App">
-      {/* <TodoList title='What is done' />
-      <TodoList title='In progress' />
-      <TodoList title='What needs to be done' /> */}
-      <Todo title="what to learn?" tasks={task} removeTodo={removeTodo}/>
-      
+      <Todo title="what to learn?" tasks={taskForTodo} removeTodo={removeTodo} changeFilter={changeFilter} addTodo={addTodo} input={input} setInput={setInput}/>
     </div>
   );
 }
--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -1,81 +1,57 @@
 import { FormEvent, useRef, useState } from "react"
-import Task from "./Task"
+import { FilterValuesType } from "../App"
 
-interface Itodo {
-    tasks: string
-    completed?: boolean
-    id?: number
-  }
 
-type TaskType = {
+
+export type TaskType = {
   id: number
-  title: string
+  text: string
   isDone: boolean
-}  
+}
 
 type PropsType = {
-  title: string,
-  tasks: Array <TaskType>
+  title: string
+  tasks: Array<TaskType>
   removeTodo: (id: number) => void
+  changeFilter: (value: FilterValuesType) => void
+  addTodo: () => void
+  setInput: any
+  input: string
 }
- 
 
-export function Todo  (props: PropsType)  {
- 
-        const [value, setValue] = useState<any>([])
-        
-        // const [todos, setTodos] = useState<Itodo[]>([userTest])
 
-        const hundlerSubmit = (e: FormEvent) => {
-          e.preventDefault()
-          console.log(value);
-          
-        }
-  
-  
-        
-        // const removeTodo = (id: number) => {
-        //   setTodo(prev => prev.filter(todo => todo.id === id))
-        // }
-      
-        // const addTodo = (title: string) => {
-        //   const newTodo: Itodo = {
-        //     title: title,
-        //     completed: false,
-        //     id: Date.now()
-        //   }
 
-        
-        //   // setTodo(prev => [newTodo, ...todo])
-        // }
 
-        // const addTodo = (task: string) => {
-              
-        //         const newTodo: Itodo = {
-        //           tasks: task,
-        //           completed: false,
-        //           id: Date.now()
-        //         }
-        //           // setTodos([...todos, newTodo])
-        // }
-      
- 
-      
-      
-        return (
-          <form onSubmit={hundlerSubmit}>
-             <input type="text" placeholder="What title?" value={value} onChange={e => setValue(e.target.value)}/>
-             <button>+</button>
-             <h3>{props.title}</h3>
-             <ul>
-              {props.tasks.map(t =>  <li>
-                <input type="checkbox" checked={t.isDone} /> <span>{t.title}</span><button onClick={() => {props.removeTodo(t.id)}}>x</button>
-                </li> )} 
-              
-             </ul>
-             
-          </form>
-        )
-      }
+export function Todo(props: PropsType) {
+
+
+  // const [todos, setTodos] = useState<Itodo[]>([userTest])
+  const [checkbox, setCheckbox] = useState(true)
+
+  const hundlerSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    props.setInput('')
+
+  }
+
+  return (
+    <form onSubmit={hundlerSubmit}>
+      <input type="text" placeholder="What title?" value={props.input} onChange={e => props.setInput(e.target.value)} />
+      <button onClick={() => props.addTodo()}>+</button>
+      <h3>{props.title}</h3>
+      <ul>
+        {props.tasks.map(t => <li>
+          <input type="checkbox" defaultChecked={checkbox}  onChange={() => setCheckbox((state) => !state)} /> <span>{t.text}</span><button onClick={() => { props.removeTodo(t.id) }}>x</button>
+        </li>)}
+
+      </ul>
+      <div>
+        <button onClick={() => props.changeFilter('all')}>All</button>
+        <button onClick={() => props.changeFilter('active')}>Active</button>
+        <button onClick={() => props.changeFilter('completed')}>Completed</button>
+      </div>
+    </form>
+  )
+}
 
 
