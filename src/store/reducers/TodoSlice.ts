3597f9d3f8ea8270eb06c9a8229a6904f16bfee5
Andrei <andrei.nakarchuk@gmail.com>
2024-04-10 01:52:48 +0300

    Create reducers

--- src/store/reducers/TodoSlice.ts
+++ src/store/reducers/TodoSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { ITodos } from "../../models/ITodos";
 
 interface todoState {
@@ -17,6 +17,16 @@ export const TodoSlice = createSlice({
     name: 'todo', 
     initialState,
     reducers: {
-
+        todoFetching: (state) => {
+            state.isLoading = true;
+        },
+        todoFetchingSuccess: (state, action: PayloadAction<ITodos[]>) => {
+            state.isLoading = false;
+            state.todos = action.payload;
+        },
+        todosFetchingError: (state, action: PayloadAction<string>) => {
+            state.isLoading = false;
+            state.error = action.payload
+        }
     }
 })
